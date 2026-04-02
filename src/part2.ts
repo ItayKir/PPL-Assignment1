dffb50318f4b82c8a705742fcc539a5f015ec8a4
Itay Kirgner <itay.kirgner@gmail.com>
2026-04-02 23:53:01 +0300

    Finished part 2

--- src/part2.ts
+++ src/part2.ts
@@ -4,11 +4,23 @@ const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+export const countVowels: (s: string) => number = R.pipe(
+    stringToArray,
+    R.filter((letter: string) => vowels.some(i => i===letter)),
+    R.length
+);;
 
 /* Question 2.2 */
-export const isPalindrome = (text: string): boolean => undefined as any;;
-  
+const checkPalindrome = (text: string): boolean => 
+    (text.length===0 || text.length===1) ? true :
+    (text[0]===text[text.length-1]) ? isPalindrome(text.slice(1,-1)) : false
+    ;;
+
+const sanitizeText = (text: string): string =>
+    text.toLowerCase().replace(/[^a-z0-9]/g,''); 
+
+export const isPalindrome = (text: string): boolean => checkPalindrome(sanitizeText(text));;
+   
 
 /* Question 2.3 */
 export type WordTree = {
@@ -16,4 +28,5 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence = (t: WordTree): string => t.children.reduce((acc, x: WordTree   ) => acc + ' ' + treeToSentence(x), t.root);;
+
