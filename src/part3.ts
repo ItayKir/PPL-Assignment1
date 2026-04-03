d090e3f199e19da207191ce6a0e42d0891bf3917
Itay Kigner <itaykirgner@gmail.com>
2026-04-03 22:07:35 +0300

    Finished returnSquaredIfFoundEven_v2

--- src/part3.ts
+++ src/part3.ts
@@ -1,4 +1,4 @@
-import { Result, makeFailure, makeOk, bind, either } from "./lib/result";
+import { Result, makeFailure, makeOk, bind, either, isOk } from "./lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -24,6 +24,7 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => undefined as any;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => bind(findResult((x) => x%2===0,a), (x) => makeOk(x*x));
+
 export const returnSquaredIfFoundEven_v3 = (a: number[]): number => undefined as any;
 
